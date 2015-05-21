30b94c99468cfd46d07742a9658b228d9ff8490c
Tom Elliott <tom.elliott@nyu.edu>
2015-05-21 18:36:44 -0500

    extract content seciton from atom feed: migrate logic from parse_xml to article

--- isaw/awol/article.py
+++ isaw/awol/article.py
@@ -28,6 +28,7 @@ class Article():
         self.id = root.find('{http://www.w3.org/2005/Atom}id').text
         self.title = unicode(root.find('{http://www.w3.org/2005/Atom}title').text)
         self.categories = root.findall('{http://www.w3.org/2005/Atom}category')
+        self.content = root.find('{http://www.w3.org/2005/Atom}content').text
         # self.content = content
         # self.url = url
         # self.blogUrl = blogUrl
