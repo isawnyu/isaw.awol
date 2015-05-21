7e0e3498114f6d46223e719ca0d79ea66a65a779
Tom Elliott <tom.elliott@nyu.edu>
2015-05-21 15:59:04 -0500

    moving csv file into data directory

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(
     author='Chuck Jones, Ronak Parpani, Tom Elliott',
     author_email='chuck.jones@nyu.edu, parpanironak@gmail.com, tom.elliott@nyu.edu',
-    data_files=[('data', ['data/awol_colon_prefixes.csv', 'awol_title_strings.csv']),],
+    data_files=[('data', ['data/awol_colon_prefixes.csv', 'awol_title_strings.csv', 'urltozot.txt']),],
     description='The Ancient World Online: from blog to bibliographic data',
     install_requires=['pyzotero', 'django', 'beautifulsoup4'],
     license='See LICENSE.txt',
