c898fc46697c47f3842b8995435eefd44cc81e97
Tom Elliott <tom.elliott@nyu.edu>
2015-05-21 16:29:03 -0500

    add docstrings in accordance with PEP0257, reformat in accordance with PEP0008, eliminate TypeError in __str__()

--- isaw/awol/article.py
+++ isaw/awol/article.py
@@ -1,6 +1,19 @@
-#Class that represents all the data that is important from the xml file
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Define classes and methods for working with AWOL blog articles.
+
+This module defines the following classes:
+ 
+ * Article: represents key information about the article.
+"""
+
 class Article:
+    """Represent all data that is important about an AWOL blog article."""
+
     def __init__(self, id, title, tags, content, url, blogUrl, issn, template):
+        """Set all data about the article."""
+
         self.id = id
         self.title = title
         self.tags = tags
@@ -11,4 +24,7 @@ class Article:
         self.issn = issn
 
     def __str__(self):
-        print self.id+"|"+self.title+"|"+str(self.tags)+"|"+self.content+"|"+self.url+"|"+self.blogUrl+"|"+self.template+"|"+self.issn
+        """Print all data about the article."""
+        return str(self.id+"|"+self.title+"|"+str(self.tags)+"|"+
+            self.content+"|"+self.url+"|"+self.blogUrl+"|"+self.template+
+            "|"+self.issn)
