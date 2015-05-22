cc607b87a144c0a40a94f71b460c961070297fc0
Tom Elliott <tom.elliott@nyu.edu>
2015-05-22 09:43:16 -0500

    requirements, datafiles, and bears oh my

--- setup.py
+++ setup.py
@@ -3,22 +3,23 @@ use_setuptools()
 
 from setuptools import setup
 
+requirements = [
+    line.strip() for line in open('REQUIREMENTS.txt')
+    if line.strip() and not line.strip().startswith('--')
+    ]
+
 
 setup(
     author='Chuck Jones, Ronak Parpani, Tom Elliott',
     author_email='chuck.jones@nyu.edu, parpanironak@gmail.com, tom.elliott@nyu.edu',
-    data_files=[('data', ['data/awol_colon_prefixes.csv', 'awol_title_strings.csv', 'urltozot.txt']),],
     description='The Ancient World Online: from blog to bibliographic data',
-    install_requires=[
-        'beautifulsoup4', 
-        'django', 
-        'lxml',
-        'nose',
-        'pyzotero', 
-        'unicodecsv'],
+    install_requires=requirements,
     license='See LICENSE.txt',
     long_description=open('README.md').read(),
     name='isaw.awol',
+    package_data={
+        '': ['*.csv']
+    },
     packages=['isaw.awol'],
     url='https://github.com/isawnyu/isaw.awol',
     version='0.1',
