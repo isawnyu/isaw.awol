e113ad8fbb042866d57f283d16daf26467f7151e
Tom Elliott <tom.elliott@nyu.edu>
2015-05-22 05:55:29 -0500

    start on a resource class to manage information about individual resources

---
+++ isaw/awol/resource.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Define classes and methods for working with resources extracted from blog.
+
+This module defines the following classes:
+
+ * Resource: Extracts and represents key information about a web resource.
+"""
+
+from bs4 import BeautifulSoup
+
+
+class Resource:
+    """Extract and represent key information about a web resource."""
+
+    def __init__(self):
+        """Set all attributes to default values."""
+        pass
+
+
+
---
+++ isaw/awol/tests/test_resource.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Test code in the resource module."""
+
+import os
+
+from nose import with_setup
+from nose.tools import *
+
+from isaw.awol import article, resource
+
+PATH_TEST = os.path.dirname(os.path.abspath(__file__))
+PATH_TEST_DATA = os.path.join(PATH_TEST, 'data')
+PATH_TEST_TEMP = os.path.join(PATH_TEST, 'temp')
+
+def setup_function():
+    """Test harness setup."""
+
+    pass
+
+def teardown_function():
+    """Test harness teardown."""
+
+    pass
+
+@with_setup(setup_function, teardown_function)
+def test_resource_init():
+    """Ensure Resource constructor works."""
+
+    r = resource.Resource()
+    assert_is_none(r.description)
+    assert_is(r.subordinate_resources, list)
+    assert_is_none(r.isbn)
+    assert_is_none(r.issn)
+    assert_is(r.keywords, list)
+    assert_is_none(r.language)
+    assert_is(r.related_resources, list)
+    assert_is_none(r.title)
+    assert_is_none(r.url)
+
