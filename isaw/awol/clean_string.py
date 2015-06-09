4bb3cf1ccb441113b6caef932a6dd0d0df9fad2e
Tom Elliott <tom.elliott@nyu.edu>
2015-06-09 12:21:07 -0500

    handle cleaning of very short strings gracefully

--- isaw/awol/clean_string.py
+++ isaw/awol/clean_string.py
@@ -50,14 +50,18 @@ def clean_string(raw):
         (u'_'),
     ]
     for j in junk:
-        if len(j) == 2:
-            cooked = cooked[1:-1] if cooked[0] == j[0] and cooked[-1] == j[1] else cooked
+        try:
+            if len(j) == 2:
+                cooked = cooked[1:-1] if cooked[0] == j[0] and cooked[-1] == j[1] else cooked
+            else:
+                cooked = cooked[1:] if cooked[0] == j[0] else cooked
+                cooked = cooked[:-1] if cooked[-1] == j[0] else cooked
+            if cooked[0:4] == u'and ':
+                cooked = cooked[4:]
+        except IndexError:
+            pass
         else:
-            cooked = cooked[1:] if cooked[0] == j[0] else cooked
-            cooked = cooked[:-1] if cooked[-1] == j[0] else cooked
-        if cooked[0:4] == u'and ':
-            cooked = cooked[4:]
-        cooked = cooked.strip()
+            cooked = cooked.strip()
     return cooked
 
 def deduplicate_lines(raws):
